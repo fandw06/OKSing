cedae532430e1401e50b606b192525562f93c4fe
Dawei Fan <fandw06@gmail.com>
2016-02-03 01:50:45 -0500

    Added a progress bar to indicate downloading process; Used SwingWorker to process listing and downloading tasks;

--- OKSing/src/dawei/oksing/OKSPanel.java
+++ OKSing/src/dawei/oksing/OKSPanel.java
@@ -1,5 +1,7 @@
 package dawei.oksing;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -36,6 +38,11 @@ public class OKSPanel extends JPanel{
 	private List<Song> songList;
 	private JFrame window;
 	private JCheckBox cb[];
+	private JCheckBox bListSongs;
+	private JButton bDownload;
+	private JLabel lSongs;
+	private JLabel lNumber;	
+	private JProgressBar pbProgress;
 	
 	/**
 	 * Create the panel.
@@ -47,9 +54,9 @@ public class OKSPanel extends JPanel{
 		this.setLocation(0, 0);
 		this.window = window;
 		String bk = "http://ok.okchang.com/home-10800746.html";
-	//	String zhuo = "http://ok.okchang.com/home-10511316.html";
-	//	String zishu = "http://ok.okchang.com/home-10362725.html";
-		person = new Person(bk);	
+		String zhuo = "http://ok.okchang.com/home-10511316.html";
+		String zishu = "http://ok.okchang.com/home-10362725.html";
+		person = new Person(zhuo);	
 		System.out.println(person.toString());
 		
 		initializeGUI();
@@ -118,148 +125,194 @@ public class OKSPanel extends JPanel{
 		
 		JLabel lName = new JLabel(person.name);
 		lName.setFont(new Font("Serif", Font.PLAIN, 35));
-		lName.setLocation(180, 10);
+		lName.setLocation(180, 15);
 		lName.setSize(160, 40);
 		add(lName);
 		
 		JLabel lFollow = new JLabel("Followers " + person.numOfFans +"    Following: "+ person.numOfFriends);
 		lFollow.setFont(new Font("Serif", Font.PLAIN, 25));
-		lFollow.setLocation(180, 60);
+		lFollow.setLocation(180, 65);
 		lFollow.setSize(420, 28);
 		add(lFollow);
 		
-		JLabel lSongs = new JLabel("Songs: "+ person.numOfSongs);
+		lSongs = new JLabel("Songs: "+ person.numOfSongs);
 		lSongs.setFont(new Font("Serif", Font.PLAIN, 25));
-		lSongs.setLocation(180, 95);
-		lSongs.setSize(280, 28);
+		lSongs.setLocation(180, 100);
+		lSongs.setSize(150, 28);
 		add(lSongs);
 		
-		JButton bListSongs = new JButton("List songs");
-		bListSongs.setFont(new Font("Serif", Font.PLAIN, 20));
-		bListSongs.setLocation(180, 130);
+		bListSongs = new JCheckBox("List songs");
+		bListSongs.setFont(new Font("Serif", Font.PLAIN, 25));
+		bListSongs.setLocation(340, 100);
 		bListSongs.setSize(150, 28);
 		bListSongs.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JPanel display = new JPanel();
-				display.setSize(200, Integer.parseInt(person.numOfSongs)*30);
-				display.setLayout(new GridLayout(Integer.parseInt(person.numOfSongs), 1));
-				display.setLocation(10, 180);
-				songList = person.listSongs();
-				cb = new JCheckBox[Integer.parseInt(person.numOfSongs)];
-				/*
-				JPanel label = new JPanel(new GridLayout(1, 3));
-				label.setSize(500, 30);
-				label.setVisible(true);
+				if (bListSongs.isSelected()) {
+					ListSongs listSongs = new ListSongs();
+					listSongs.execute();
+					
+				}
 
-				label.add(new JLabel("   Name"));
-				label.add(new JLabel("Times"));
-				label.add(new JLabel("Upload time"));
-				display.add(label);
-				*/
-				for (int i = 0; i< songList.size(); i++) {
-					Song song = songList.get(i);
-					System.out.println(song.toString());
-					
-					JPanel row = new JPanel(new GridLayout(1, 3));
-					row.setSize(330, 30);
-					row.setLocation(0, 0);
-					row.setVisible(true);
-					
-					cb[i] = new JCheckBox(song.title+"   ");
-					row.add(cb[i]);
-					row.add(new JLabel(song.numOfPlayed));
-					row.add(new JLabel(song.uploadTime));
-					display.add(row);
-				}	
-				
-				JScrollPane spSongsList = new JScrollPane(display);
-				spSongsList.setSize(565, 300);
-				spSongsList.setLocation(10, 180);
-				spSongsList.setVisible(true);
-				spSongsList.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-				OKSPanel.this.add(spSongsList);
-				spSongsList.validate();
-				display.validate();
-				//OKSPanel.this.validate();
 				
 			}
 			
 		});
 		add(bListSongs);
 		
-		JButton bDownload = new JButton("Download");
+		bDownload = new JButton("Download");
 		bDownload.setFont(new Font("Serif", Font.PLAIN, 20));
-		bDownload.setLocation(350, 130);
+		bDownload.setLocation(180, 135);
 		bDownload.setSize(150, 28);
+		bDownload.setEnabled(false);
 		bDownload.addActionListener(new ActionListener(){
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				for (int i = 0; i< cb.length; i++) {
-					cb[i].setEnabled(false);
-				}
-				
-				for (int i = 0; i< cb.length; i++) {
-					if (cb[i].isSelected()) {
-						Song song = songList.get(i);
-						System.out.println(i);
-						URL website = song.getMediaAddress();
-						int fileSize = Crawler.getFileSize(website);
-						System.out.println("File Size: "+ fileSize);
-						try {
-							ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-							FileOutputStream fos = new FileOutputStream(song.title+".m4a");
-							int bufferSize = 60000;
-						//	fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-							
-							ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-							int bytesRead = rbc.read(buffer);
-							int loop = 0;
-							
-							while(bytesRead != -1 ) {
-								loop++;
-								buffer.flip();
-								//		System.out.println("After Flip: Position: "+buf.position()+ " limit: "+buf.limit());
-								while(buffer.hasRemaining())
-									fos.write(buffer.get());
-								if (loop % 10 == 0) {
-									System.out.println("Progress "+ bufferSize*loop);
-									System.out.format("progress: %2.2f%%\n", (double)10240*100*loop/(double)fileSize<100? (double)10240*100*loop/(double)fileSize:100);
-								}
-								buffer.clear();
-							    bytesRead = rbc.read(buffer);
-							}
-							System.out.println("Downloaded: "+ loop*bufferSize);
-							fos.close();
-						} catch (IOException e1) {
-							e1.printStackTrace();
-						}
-					}
-				}
-				
-				for (int i = 0; i< cb.length; i++) {
-					cb[i].setEnabled(true);
-				}
-				
+				Download download = new Download();
+				download.execute();		
 			}
 			
 		});
 		add(bDownload);
+		
+		lNumber = new JLabel("No.");
+		lNumber.setFont(new Font("Serif", Font.PLAIN, 22));
+		lNumber.setLocation(345, 127);
+		lNumber.setSize(40, 40);
+		add(lNumber);
+		
+		pbProgress = new JProgressBar(0, 100);
+	    pbProgress.setValue(0);
+	    pbProgress.setStringPainted(true);
+	    pbProgress.setSize(160, 25);
+	    pbProgress.setLocation(380, 136);
+	    add(pbProgress);
 	}
 
-	private class Download extends SwingWorker<Void, Void> {
+	private class Download extends SwingWorker<Void, int[]> {
 
 		@Override
 		protected Void doInBackground() throws Exception {
+			bDownload.setText("Downloading");
+			bDownload.setEnabled(false);
+			for (int i = 0; i< cb.length; i++) {
+				cb[i].setEnabled(false);
+			}
+			
+			for (int i = 0; i< cb.length; i++) {
+				if (cb[i].isSelected()) {
+					Song song = songList.get(i);
+					System.out.println(i);
+					URL website = song.getMediaAddress();
+					int fileSize = Crawler.getFileSize(website);
+					System.out.println("File Size: "+ fileSize);
+					try {
+						ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+						FileOutputStream fos = new FileOutputStream(song.title+".m4a");
+						int bufferSize = 60000;
+					//	fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+						
+						ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+						int bytesRead = rbc.read(buffer);
+						int loop = 0;
+						
+						while(bytesRead != -1 ) {
+							
+							buffer.flip();
+							//		System.out.println("After Flip: Position: "+buf.position()+ " limit: "+buf.limit());
+							while(buffer.hasRemaining())
+								fos.write(buffer.get());
+							loop++;
+							if (loop % 10 == 0) {
+								int percent = (int)((double)10240*100*loop/(double)fileSize<100? (double)10240*100*loop/(double)fileSize:100);
+								publish(new int[]{i+1, percent});
+								System.out.println("Progress "+ bufferSize*loop);
+								System.out.format("progress: %2.2f%%\n", (double)10240*100*loop/(double)fileSize<100? (double)10240*100*loop/(double)fileSize:100);
+							}
+							
+							buffer.clear();
+						    bytesRead = rbc.read(buffer);
+						}
+						System.out.println("Downloaded: "+ loop*bufferSize);
+						fos.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+			
 			
 			return null;
 		}
 		
 		@Override
 		protected void done() {
+			bDownload.setText("Download");
+			bDownload.setEnabled(true);
+			for (int i = 0; i< cb.length; i++) {
+				cb[i].setSelected(false);
+				cb[i].setEnabled(true);
+			}
+		}
+		
+		@Override
+		protected void process(List<int[]> p) {
+			int current = p.get(p.size()-1)[0];
+			int percent = p.get(p.size()-1)[1];
+			pbProgress.setValue(percent);
+			lNumber.setText(Integer.toString(current));
+		}
+		
+	}
+	
+	private class ListSongs extends SwingWorker<Void, Void> {
+
+		@Override
+		protected Void doInBackground() throws Exception {
 			
+			lSongs.setText("Listing...");
+			JPanel display = new JPanel();
+			display.setSize(200, Integer.parseInt(person.numOfSongs)*30);
+			display.setLayout(new GridLayout(Integer.parseInt(person.numOfSongs), 1));
+			display.setLocation(10, 180);
+			songList = person.listSongs();
+			cb = new JCheckBox[Integer.parseInt(person.numOfSongs)];
+
+			for (int i = 0; i< songList.size(); i++) {
+				Song song = songList.get(i);
+				System.out.println(song.toString());
+				
+				JPanel row = new JPanel(new GridLayout(1, 3));
+				row.setSize(330, 30);
+				row.setLocation(0, 0);
+				row.setVisible(true);
+				
+				cb[i] = new JCheckBox(song.title+"   ");
+				row.add(cb[i]);
+				row.add(new JLabel(song.numOfPlayed));
+				row.add(new JLabel(song.uploadTime));
+				display.add(row);
+			}	
+			
+			JScrollPane spSongsList = new JScrollPane(display);
+			spSongsList.setSize(565, 300);
+			spSongsList.setLocation(10, 180);
+			spSongsList.setVisible(true);
+			spSongsList.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			OKSPanel.this.add(spSongsList);
+			spSongsList.validate();
+			display.validate();
+			//OKSPanel.this.validate();
+			bListSongs.setEnabled(false);
+			return null;
+		}
+		
+		@Override
+		protected void done() {
+			lSongs.setText("Songs: "+ person.numOfSongs);
+			bDownload.setEnabled(true);
 		}
 		
 	}
